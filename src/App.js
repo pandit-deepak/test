4812cb160ed1a8054fcda2583f075fae5365cfcf
Deepak Pandit <deepakpandit45678@gmail.com>
2024-06-01 21:54:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Testimonials from "./components/Testimonials.js";
+import reviews from './data';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col w-[100vw] h-[100vh] items-center bg-teal-300">
+        <h1>Our Testimonials</h1>
+        <div></div>
+        <Testimonials review={reviews}/>
     </div>
   );
 }
